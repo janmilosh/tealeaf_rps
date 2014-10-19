c1f3fc18a4cab8793c890c19dcf456aef0c519a7
Jan Milosh <janmilosh@gmail.com>
2014-10-19 19:56:44 -0400

    Post code review refactor

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,49 +1,39 @@
-puts "Play Paper Rock Scissors!"
+puts "Play Rock Paper Scissors!"
 
 CHOICES = {'r' => 'Rock', 'p' => 'Paper', 's' => 'Scissors'}
 
-def evaluate(user_input, computer_input)
+def display_results(user_input, computer_input)
   puts "You picked #{user_input} and computer picked #{computer_input}."
   if user_input == computer_input
     puts "It's a tie."
-  elsif user_input == 'Rock' and computer_input == 'Paper'
+  elsif user_input == 'Rock' && computer_input == 'Paper'
     puts "Paper wraps Rock!\nComputer Won!"
-  elsif user_input == 'Paper' and computer_input == 'Scissors'
+  elsif user_input == 'Paper' && computer_input == 'Scissors'
     puts "Scissors cuts paper!\nComputer Won!"
-  elsif user_input == 'Scissors' and computer_input == 'Rock'
+  elsif user_input == 'Scissors' && computer_input == 'Rock'
     puts "Rock crushes scissors!\nComputer Won!"
-  elsif user_input == 'Rock' and computer_input == 'Scissors'
+  elsif user_input == 'Rock' && computer_input == 'Scissors'
     puts "Rock crushes scissors!\nYou Won!"
-  elsif user_input == 'Paper' and computer_input == 'Rock'
+  elsif user_input == 'Paper' && computer_input == 'Rock'
     puts "Paper wraps Rock!\nYou Won!"
-  elsif user_input == 'Scissors' and computer_input == 'Paper'
+  elsif user_input == 'Scissors' && computer_input == 'Paper'
     puts "Scissors cuts paper!\nYou Won!"
   end
 end
 
-def get_user_input
-  puts "Choose one: (P/R/S)"
-  user_input = gets.chomp
-  CHOICES[user_input.downcase]
-end
-
 def play_game
-  loop do
-    user_input = get_user_input
-    if user_input != nil
+  begin
+    puts "Choose one: (P/R/S)"
+    user_input = CHOICES[gets.chomp.downcase]
+    if user_input
       computer_input = CHOICES.values.sample
-      evaluate(user_input, computer_input)
+      display_results(user_input, computer_input)
     else
       puts "That wasn't a valid input."
     end
     puts "Keep playing? (Y/N)"
-    continue = gets.chomp
-    if continue.downcase == 'y'
-      continue
-    else
-      break
-    end
-  end
+    keep_playing_response = gets.chomp.downcase    
+  end while keep_playing_response == 'y'
 end
 
 play_game
