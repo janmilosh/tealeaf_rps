8393bae744df12e8151e1ef4556680ad795c206a
Jan Milosh <janmilosh@gmail.com>
2014-10-19 11:06:03 -0400

    Refactors to use a hash for choices

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,22 +1,10 @@
 puts "Play Paper Rock Scissors!"
 
-
-def get_long_input(input)
-  case input.downcase
-  when 'r'
-    return 'Rock'
-  when 'p'
-    return 'Paper'
-  when 's'
-    return 'Scissors'
-  else
-    return false
-  end
-end
+CHOICES = {'r' => 'Rock', 'p' => 'Paper', 's' => 'Scissors'}
 
 def evaluate(user_input, computer_input)
   puts "You picked #{user_input} and computer picked #{computer_input}."
-  if user_input == coomputer_input
+  if user_input == computer_input
     puts "It's a tie."
   elsif user_input == 'Rock' and computer_input == 'Paper'
     puts "Paper wraps Rock!\nComputer Won!"
@@ -33,22 +21,17 @@ def evaluate(user_input, computer_input)
   end
 end
 
-def get_computer_input
-  choices = ['Rock', 'Paper', 'Scissors']
-  return choices.sample
-end
-
 def get_user_input
   puts "Choose one: (P/R/S)"
   user_input = gets.chomp
-  get_long_input(user_input)
+  CHOICES[user_input.downcase]
 end
 
 def play_game
   loop do
     user_input = get_user_input
-    if user_input != false
-      computer_input = get_computer_input
+    if user_input != nil
+      computer_input = CHOICES.values.sample
       evaluate(user_input, computer_input)
     else
       puts "That wasn't a valid input."
