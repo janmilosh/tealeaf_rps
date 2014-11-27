fc7993b53a433c70e92cd21ad1232ac0fcb8ef2e
Jan Milosh <janmilosh@gmail.com>
2014-11-27 16:04:21 -0500

    Adds small format tweaks

--- rock_paper_scissors_lesson2.rb
+++ rock_paper_scissors_lesson2.rb
@@ -44,7 +44,7 @@ class Game
 
   def declare_winner(player1, player2)
     system "clear"
-    puts "#{player1.name} picked #{weapon1} and #{player2.name} picked #{weapon2}."
+    puts "#{player1.name} picked #{weapon1} and #{player2.name} picked #{weapon2}.\n\n"
     if weapon1 == weapon2
       puts "It's a tie."
     elsif weapon1 == 'r' && weapon2 == 'p'
@@ -78,14 +78,13 @@ class Game
   end
 
   def total_wins(player1, player2)
-    puts
-    puts "Total wins:"
+    puts "\n   Total Wins   "
+    puts "~~~~~~~~~~~~~~~~"
     puts "#{player1.name}: #{player1.number_of_wins}"
-    puts "#{player2.name}: #{player2.number_of_wins}"
+    puts "#{player2.name}: #{player2.number_of_wins}\n\n"
   end
 
   def keep_playing?
-    puts
     puts "Keep playing? (y/n)"
     gets.chomp.downcase
   end
@@ -94,7 +93,8 @@ end
 def play_game
   system "clear"
   puts "Play Paper, Rock, Scissors"
-  
+  puts "~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
+
   puts "Please enter your name ==> "
   player1 = Player.new(gets.chomp)
   player2 = Player.new("The Computer")
